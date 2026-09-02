28a04643db425fa7edbfd5f6ea5d38d57a271c8c
govardhan2305 <gova98989@gmail.com>
2026-09-03 00:53:46 +0530

    Initial Spring Boot backend setup

---
+++ backend/src/test/java/ai_engineering_copilot/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ai_engineering_copilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
